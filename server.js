5a2d1da59591c91f7dcb1aea1ee43a2fe59a9b8e
Ricardo Camacho <ing.camacho29@gmail.com>
2025-03-20 00:15:30 -0600

    se agregan middleware cors y morgan

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import 'dotenv/config.js'
 import './config/database.js'
+import cors from 'cors';
+import morgan from 'morgan';
 
 
 const server = express();
@@ -8,5 +10,12 @@ const server = express();
 const PORT = process.env.PORT || 8080;
 
 const ready = () => console.log("server is ready on port :" + PORT);
+
+//configuraciones basicas
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
+server.use(cors());
+server.use(morgan('dev'));
+
 
 server.listen(PORT, ready);
