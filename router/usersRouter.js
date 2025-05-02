d77b21638c2f772d18cc85760a111be6930d9bd4
Ricardo Camacho <ing.camacho29@gmail.com>
2025-05-02 00:30:33 -0600

    se agrega el middleware para hashear el passworrd

--- router/usersRouter.js
+++ router/usersRouter.js
@@ -6,6 +6,7 @@ import remove from '../controllers/users/delete.js';
 import schemaUser from '../schema/users/create.js';
 import validator from '../middleware/validator.js';
 import accountExist from "../middleware/accountExist.js";
+import createHash from "../middleware/createHash.js";
 
 
 
@@ -16,8 +17,8 @@ routerUser.get('/userByEmail/:emailParams', userByEmail);
 routerUser.get('/userById/:idParams', userById);
 routerUser.get('/userByCountry/:country', userByCountry);
 routerUser.post('/createMany', createMany);
-routerUser.post('/createOne', validator(schemaUser), accountExist, Register);
-routerUser.put('/update',validator(schemaUser), update);
+routerUser.post('/createOne', validator(schemaUser), accountExist, createHash, Register);
+routerUser.put('/update',validator(schemaUser),accountExist, update);
 routerUser.delete('/delete/:id', remove);
 
 export default routerUser;
