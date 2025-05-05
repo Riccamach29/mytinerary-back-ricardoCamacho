7d17fb95be66157be6cbb970f597b336a48f20b1
Ricardo Camacho <ing.camacho29@gmail.com>
2025-05-04 20:07:03 -0600

    Se termina el signout

--- router/auth.js
+++ router/auth.js
@@ -5,10 +5,15 @@ import validator from '../middleware/validator.js';
 import accountNotExist from '../middleware/accountNotExist.js';
 import validatedPassword from '../middleware/validatedPassword.js';
 import generateToken from '../middleware/generateToken.js';
+import passport from '../middleware/passport.js';
+import signOut from '../controllers/auth/signOut.js';
 
 
 const routerAuth = Router();
 
-routerAuth.post('/signIn', validator(schemaSignIn),accountNotExist, validatedPassword, generateToken, signIn);
+routerAuth.post('/signIn', validator(schemaSignIn), accountNotExist, validatedPassword, generateToken, signIn);
+routerAuth.post('/signOut', passport.authenticate('jwt', { session: false }), signOut);
+
+
 
 export default routerAuth;
